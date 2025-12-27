671cc2e2c113082a54b6fb8e1e0703a39270d08f
Akbarali Tursunboyev <tursunboyevakbarali807@gmail.com>
2025-12-27 20:16:44 +0500

    Update user.js

--- server/user.js
+++ server/user.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   telegramId: { type: Number, required: true, unique: true },
@@ -9,4 +9,6 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
